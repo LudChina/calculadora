3bf699c5d6bc8d83fb997f393cf66c12e54d0bf9
yagok <yago_kolodij@hotmail.com>
2023-01-06 15:44:47 -0300

    iniciar

---
+++ script.js
@@ -0,0 +1,6 @@
+function sumar(){
+    let n1 =parseInt(document.getElementById("num1").value)
+    let n2 = parseInt(document.getElementById("num2").value)
+    
+    document.getElementById("result").innerHTML = (n1+n2)
+}

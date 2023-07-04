ab2e65380e52e1aaae9839d30dbd765ba85ea1b6
LudChina <110573856+LudChina@users.noreply.github.com>
2023-07-04 11:47:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function sumar(){
     
     document.getElementById("result").innerHTML = (n1+n2)
 }
+
+function restar(){
+    let n1 =parseInt(document.getElementById("num1").value)
+    let n2 = parseInt(document.getElementById("num2").value)
+    
+    document.getElementById("result").innerHTML = (n1-n2)
+}
+
+function multiplicar(){
+    let n1 =parseInt(document.getElementById("num1").value)
+    let n2 = parseInt(document.getElementById("num2").value)
+    
+    document.getElementById("result").innerHTML = (n1*n2)
+}
+
+function dividir(){
+    let n1 =parseInt(document.getElementById("num1").value)
+    let n2 = parseInt(document.getElementById("num2").value)
+    
+    document.getElementById("result").innerHTML = (n1/n2)
+}
